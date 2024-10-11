edfc2fef36505c072c3b07c06467c0262a49e21b
KhantTiKyiSectionD <N01709727@humber.ca>
2024-10-10 18:34:40 -0800

    Add files via upload

---
+++ node.js
@@ -0,0 +1,24 @@
+let myNumber = 10;
+let myString = "Hello, JavaScript!";
+
+console.log(myNumber);
+console.log(myString);
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 5; i++) {
+  console.log(3 * i);
+}
+
+function addingNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addingNumbers(5, 7));
+
+function multiplyingNumbers(num1, num2) {
+  return num1 * num2;
+}
+console.log(multiplyingNumbers(4, 6));
